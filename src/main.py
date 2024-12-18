d90d8d8101246bbaa156b924a0222d78c98762a0
Robert Kirchner <rjojjr91@live.com>
2024-12-18 12:25:26 -0600

    add cmd to list all active hostnames

--- src/main.py
+++ src/main.py
@@ -5,13 +5,8 @@ import time
 
 version = '1.0.1'
 
-def main():
-    print(f'ClouDNS DyDNS Update Client')
-    print('----------------------------')
-    print(f'Version: {version}')
-    print('2024 Robert Kirchner JR(rjojjr)')
-    print('----------------------------')
-    print()
+
+def _application():
     if len(sys.argv) == 4 and (sys.argv[1] == '-a' or sys.argv[1] == '--add-hostname'):
         print(f'Adding domain {sys.argv[2]} with update URL {sys.argv[3]}')
         state_manager.add_entry(sys.argv[2], sys.argv[3])
@@ -26,6 +21,13 @@ def main():
         print(f'Set update interval minutes to {sys.argv[2]}')
         return
 
+    if len(sys.argv) == 2 and (sys.argv[1] == '-lh' or sys.argv[1] == '--list-hostnames'):
+        print(f'Listing currently active hostnames')
+        state = state_manager.get_state()
+        for host in state:
+            printf(f'Hostname: {host["domain"]}, Added At: {host["addedAt"]}')
+        return
+
     print('Starting DyDNS update thread')
     while True:
         print('Updating ClouDNS domains')
@@ -34,6 +36,16 @@ def main():
         print('Updated ClouDNS domains')
         interval = state_manager.get_config()['updateIntervalMinutes'] * 60
         time.sleep(interval)
+
+
+def main():
+    print(f'ClouDNS DyDNS Update Client')
+    print('----------------------------')
+    print(f'Version: {version}')
+    print('2024 Robert Kirchner JR(rjojjr)')
+    print('----------------------------')
+    print()
+    _application()
 
 
 if __name__ == '__main__':
